3bf4da2da49ba739111668dc88d034b10a541394
VenTaz <40668720+VenTaz@users.noreply.github.com>
2021-04-04 00:54:55 +0200

    Update main.cpp

--- BACASABLE/BACASABLE/main.cpp
+++ BACASABLE/BACASABLE/main.cpp
@@ -5,15 +5,17 @@ int main()
 {
 	SetConsoleTitleA("");
 	std::printf("[+] welcome to BACASABLE, a free BAC (Badlion AntiCheat) HWID (hardware identifier) spoofer...\n\n");
-	system("pause");
+	std::system("pause");
 
 	std::printf("[+] clearing display monitors information from registry...\n");
 	system("reg delete HKLM\\SYSTEM\\ControlSet001\\Enum\\Display /f");
 	system("reg delete HKLM\\SYSTEM\\CurrentControlSet\\Enum\\Display /f");
 
-	std::printf("[+] spoofing disk identifiers...\n");
+	std::printf("\n[+] spoofing disk identifiers...\n\n");
 	system("kdmapper.exe spoofer.sys");
 
-	std::printf("[+] done...\n");
+	std::printf("\n[+] done...\n");
+	std::system("pause > nul");
+	
 	return 0;
 }
